3ca0889cd1b6c5aa18f45bdaadacab434d6c0b8e
Lee Byungeun <byungeun96@gmail.com>
2024-05-24 15:32:49 +0900

    Spring Security 권한 만들기

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,16 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-validation
-	implementation("org.springframework.boot:spring-boot-starter-validation:3.2.1")
+	// Spring Starter Validation
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 
+	// Spring Security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// JTW
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
