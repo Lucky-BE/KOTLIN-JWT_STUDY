4b63837a3c82c73044179d6af9505b2a6876035b
Lee Byungeun <byungeun96@gmail.com>
2024-05-24 16:50:50 +0900

    CustomUser 로 Token 에 userId 관리기능 추가

--- src/main/kotlin/com/example/demo/common/authority/JwtTokenProvider.kt
+++ src/main/kotlin/com/example/demo/common/authority/JwtTokenProvider.kt
@@ -1,5 +1,6 @@
 package com.example.demo.common.authority
 
+import com.example.demo.common.dto.CustomUser
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.Jwts
@@ -43,6 +44,7 @@ class JwtTokenProvider {
             .builder()
             .setSubject(authentication.name)
             .claim("auth", authorities)
+            .claim("userId", (authentication.principal as CustomUser).userId)
             .setIssuedAt(now)
             .setExpiration(accessExpiration)
             .signWith(key, SignatureAlgorithm.HS256)
@@ -58,13 +60,14 @@ class JwtTokenProvider {
         val claims: Claims = getClaims(token)
 
         val auth = claims["auth"] ?: throw RuntimeException("잘못된 토큰입니다.")
+        val userId = claims["userId"] ?: throw RuntimeException("잘못된 토큰입니다.")
 
         // 권한 정보 추출
         val authorities: Collection<GrantedAuthority> = (auth as String)
             .split(",")
             .map { SimpleGrantedAuthority(it) }
 
-        val principal: UserDetails = User(claims.subject, "", authorities)
+        val principal: UserDetails = CustomUser(userId.toString().toLong(), claims.subject, "", authorities)
 
         return UsernamePasswordAuthenticationToken(principal, "", authorities)
     }
